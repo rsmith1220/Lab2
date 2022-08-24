85128845976132103507fbc37843f53fb06b9185
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2022-08-23 18:49:11 -0600

    3 shaderes

--- SmithRebecca_Lab2.py
+++ SmithRebecca_Lab2.py
@@ -6,7 +6,7 @@ from textures import Texture
 from obj import Obj
 import random
 
-from shaders import flat,unlit, gourad, toon, glow, textureBlend
+from shaders import flat,unlit, gourad, toon, glow, textureBlend, sabor
 
 
 w=700
@@ -19,12 +19,32 @@ rend.dirLight = V3(1,0,0)
 
 rend.active_texture = Texture("body.bmp")
 rend.active_texture2 = Texture("shades.bmp")
-rend.active_shader = flat
+rend.active_shader = sabor
 
 
 rend.glLoadModel("cookie.obj",
                 translate = V3(0, 0, -10),
-                scale = V3(2,2,2),
+                scale = V3(1,1,1),
+                rotate = V3(0, 65, 90)
+                )
+
+rend.active_texture = Texture("body.bmp")
+rend.active_texture2 = Texture("shades.bmp")
+rend.active_shader = toon
+
+rend.glLoadModel("cookie.obj",
+                translate = V3(1, 0, -10),
+                scale = V3(1,1,1),
+                rotate = V3(0, 65, 90)
+                )
+
+rend.active_texture = Texture("body.bmp")
+rend.active_texture2 = Texture("shades.bmp")
+rend.active_shader = textureBlend
+
+rend.glLoadModel("cookie.obj",
+                translate = V3(0.5, 0, -10),
+                scale = V3(1,1,1),
                 rotate = V3(0, 65, 90)
                 )
 
--- shaders.py
+++ shaders.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matesRS
 
 def flat(render, **kwargs):
     # Normal calculada por poligono
@@ -254,3 +255,40 @@ def textureBlend(render, **kwargs):
     if r < 0: r = 0
 
     return r, g, b
+
+def sabor(render, **kwargs):
+    u, v, w = kwargs["baryCoords"]
+    b, g, r = kwargs["vColor"]
+    tA, tB, tC = kwargs["texCoords"]
+    nA, nB, nC = kwargs["normals"]
+
+    b /= 255
+    g /= 255
+    r /= 255
+    
+    if render.active_texture:
+        # P = Au + Bv + Cw
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+
+        texColor = render.active_texture.getColor(tU, tV)
+
+        b *= texColor[2]
+        g *= texColor[1]
+        r *= texColor[0]
+
+    triangleNormal = np.array([nA[0] * u + nB[0] * v + nC[0] * w,
+                               nA[1] * u + nB[1] * v + nC[1] * w,
+                               nA[2] * u + nB[2] * v + nC[2] * w])
+
+    dirLight = np.array(render.dirLight)
+    intensity = np.dot(triangleNormal, -dirLight)
+
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    if intensity > 0:
+        return r, g, b
+    else:
+        return 0,0,0
